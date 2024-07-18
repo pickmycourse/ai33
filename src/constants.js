90d7623ff45f895df9bd64a25b4cede704e9f935
pickmycourse <tech@pickmycourse.io>
2024-07-18 19:25:39 +0530

    1st

---
+++ src/constants.js
@@ -0,0 +1,52 @@
+//BRAND
+export const name = 'PickMyCourse';
+export const company = 'SeenIT PTY LTD';
+export const websiteURL = 'http://localhost:3000';
+export const serverURL = 'http://localhost:5000';
+export const logo = 'https://firebasestorage.googleapis.com/v0/b/pick-my-course-ai.appspot.com/o/PMC_logos.png?alt=media&token=e4c3ed9b-c085-4162-ad6b-1e889b36b4bf';
+export const razorpayEnabled = false;
+export const paypalEnabled = false;
+export const stripeEnabled = true;
+export const paystackEnabled = false;
+
+//PRICING :-
+
+//FREE 
+export const FreeType = 'Free Plan';
+export const FreeCost = 0;
+export const FreeTime = '';
+
+//MONTHLY 
+export const MonthType = 'Monthly Plan';
+export const MonthCost = 1;
+export const MonthTime = 'month';
+
+//YEARLY 
+export const YearType = 'Yearly Plan';
+export const YearCost = 10;
+export const YearTime = 'year';
+
+//TESTIMONIAL
+export const review = "The AI Course Generator revolutionized my content creation process, providing accurate and relevant topics effortlessly. It's a time-saving powerhouse that enhances the quality and relevance of my courses. A must-have tool for educators seeking efficiency and impactful online learning experiences.";
+export const from = "Dinesh Kumar";
+export const profession = 'Software Developer';
+export const photoURL = 'https://media.licdn.com/dms/image/D5603AQEDjPydFO9Z2A/profile-displayphoto-shrink_100_100/0/1683617907233?e=1726704000&v=beta&t=uewK1iXPFGiN0pS1XlqLgoM_aQgW7gz7THrHLttayCk';
+
+//PAYPAL
+export const paypalPlanIdOne = "";
+export const paypalPlanIdTwo = "";
+
+//RAZORPAY
+export const razorpayKeyId = "";
+export const razorpayPlanIdOne = "";
+export const razorpayPlanIdTwo = "";
+
+//STRIPE
+export const stripePlanIdOne = "price_1Pd8mq01PbsRdqnLHOa9bhU1";
+export const stripePlanIdTwo = "price_1PduXT01PbsRdqnLVEkLsDGb";
+
+//PAYSTACK
+export const paystackPlanIdOne = "";
+export const paystackPlanIdTwo = "";
+export const amountInZarOne = '';
+export const amountInZarTwo = '';
